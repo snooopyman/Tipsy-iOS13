7ec7c7517730691d7203a6cc2f3dfb700d086ebe
Snooopyman <armando.snoop@gmail.com>
2023-05-09 21:41:08 +0200

    creating IBOutlets and IBActions

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
     }
-
-
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
